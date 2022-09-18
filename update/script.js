7a01aa89da9a66270dd2b3d020f7d5a15f6a157c
Rotwo <Alvarotwoc2008@outlook.es>
2022-09-18 15:24:40 +0200

    Add files via upload

---
+++ update/script.js
@@ -0,0 +1,12 @@
+var downloadBtn = document.getElementById('downloadAppBtn');
+
+downloadBtn.addEventListener('click', () => {
+    window.open("https://raw.githubusercontent.com/Rotwo/Discord-Custom-Status/master/dist/%F0%9F%92%AB%20Discord%20Custom%20Status%20%F0%9F%92%A6%20Setup%201.0.0.exe", "_blank");
+});
+
+var gitReleases = document.getElementById('gitReleases');
+
+gitReleases.addEventListener('click', () => {
+    console.log('Github Releases');
+    window.open('https://github.com/Rotwo/Discord-Custom-Status/releases', "_blank")
+});
